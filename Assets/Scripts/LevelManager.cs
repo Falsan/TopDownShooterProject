0ef53aad0cd511a6216134f0cde8d1f955988272
Falsan <sam.britnell117@gmail.com>
2016-12-13 09:56:34 +0000

    Just a few useful scripts we may be able to use

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour {
+
+    private static LevelManager m_instance;
+    [SerializeField]
+    GameObject testLevelReference;
+    [SerializeField]
+    GameObject playerReference;
+
+    [SerializeField]
+    GameObject testEnemyRef;
+
+    [SerializeField]
+    List<GameObject> enemyPool;
+
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject loadedLevel;
+
+	// Use this for initialization
+	void Start () {
+
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+        else if (m_instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        createTestEnvironment();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void createTestEnvironment()
+    {
+        loadedLevel = testLevelReference;
+        Instantiate(loadedLevel);
+
+        player = playerReference;
+        Instantiate(player);
+        Vector3 playerPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        player.transform.position = playerPosition;
+        Vector3 spawnPos = new Vector3(-1.984097f, -0.01800001f, 0.0f);
+        enemyPool.Add(Instantiate(testEnemyRef));
+
+        enemyPool[0].transform.position = spawnPos;
+
+        
+    }
+}
---
+++ Assets/Scripts/PlayerFireScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFireScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject bulletRef;
+
+    enum fireAngles
+    {
+        up = 0,
+        left = 1,
+        right = 2
+    }
+    int m_fireAngle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            m_fireAngle = (int)fireAngles.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_fireAngle = (int)fireAngles.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_fireAngle = (int)fireAngles.right;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            //pew
+            Instantiate(bulletRef).
+                GetComponent<BulletScript>().
+                setDirectionAndPosition(gameObject, m_fireAngle);
+        }
+    }
+}
